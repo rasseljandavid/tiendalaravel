d9ab190462fe227368f078c0dd7fb762fa371167
= <rasseljandavid@gmail.com>
2016-09-27 05:27:15 +0800

    add custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('metainfo')
+	<title>Page Not Found : Tienda -Your First Online Grocery in the Philippines</title>
+  <meta name="description" content="About Us : Tienda -Your First Online Grocery in the Philippines">
+@endsection
+
+@section('content')
+    <div class="container">
+      <div class="row">
+        <!--Middle Part Start-->
+        <div id="content" class="col-sm-12">
+          <h1 class="title-404 text-center">404</h1>
+          <p class="text-center lead">Sorry!<br>
+            The page you requested cannot be found! </p>
+          <div class="buttons text-center"> <a class="btn btn-primary btn-lg btn-red" href="/">Continue</a> </div>
+        </div>
+        <!--Middle Part End -->
+      </div>
+    </div>
+@endsection
