89c4694f6a779aa3293b2b8dc2af134590c26289
= <rasseljandavid@gmail.com>
2016-09-30 02:57:19 +0800

    Make the product images in the portrait dynamic

--- resources/views/products/portrait.blade.php
+++ resources/views/products/portrait.blade.php
@@ -1,7 +1,7 @@
 <div class="product-thumb clearfix">
   <div class="image">
     <a href="{{ $product->slugLink() }}">
-      <img src="/image/products/Champi.jpg" alt="{{ $product->title }}" title="{{ $product->title }}" class="img-responsive" />
+      <img src="/image/products/{{ $product->id }}.jpg" alt="{{ $product->title }}" title="{{ $product->title }}" class="img-responsive" />
     </a>
   </div>
   <div class="caption">
