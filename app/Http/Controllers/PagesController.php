dbcfb5842788657367e9920b277d57e5cb00d845
Rassel Jan David <rasseljandavid@gmail.com>
2016-11-11 07:39:07 +0400

    Clean the router file for the sending of email

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use Mail;
+
 class PagesController extends Controller
 {
     public function about() {
@@ -34,5 +36,84 @@ class PagesController extends Controller
 
     public function sitemap() {
     	return view('pages.sitemap');
+    }
+
+    public function sendemail() {
+        $field_rules = array(
+        'name' => 'required',
+        'email'   => 'required|valid_email',
+        'message' => 'required'
+        );
+
+        // change your error messages here
+        $error_messages = array(
+            'required'    => 'This field is required',
+            'valid_email' => 'Please enter a valid email address'
+        );
+
+        $error_placements = array();
+
+        // success message
+        $success_message            = new \stdClass();
+        $success_message->message   = 'Thank you! Your message has been sent.';
+        $success_message->field     = 'submitButton';
+
+        // mail failure message
+        $mail_error_message            = new \stdClass();
+        $mail_error_message->message   = 'Sorry your mail was not sent - please try again later';
+        $mail_error_message->field     = 'submitButton';
+
+        $fields = $_POST;
+
+        $returnVal           = new \stdClass();
+        $returnVal->status   = 'error';
+        $returnVal->messages = array();
+
+        if (!empty($fields)) {
+            //Validate each of the fields
+            foreach ($field_rules as $field => $rules) {
+                $rules = explode('|', $rules);
+
+                foreach ($rules as $rule) {
+                    $result = null;
+
+                    if (isset($fields[$field])) {
+                        if (!empty($rule)) {
+                            $result = $rule($fields[$field]);
+                        }
+
+                        if ($result === false) {
+                            $error = new \stdClass();
+                            $error->field = $field;
+                            $error->message = $error_messages[$rule];
+                            $error->placement = $error_placements[$field];
+
+                            $returnVal->messages[] = $error;
+                            // break from the rule loop so we only get 1 error at a time
+                            break;
+                        }
+                    } else {
+                        $returnVal->messages[] =  $field . ' ' . $error_messages['required'];
+                    }
+                }
+            }
+
+            if (empty($returnVal->messages)) {                         // Enable encryption, 'ssl' also accepted
+                $email = stripslashes(safe($fields['email']));
+                $body = stripslashes(safe($fields['message']));
+                $content = $email . " sent you a message from your contact form:<br><br>";
+                $content .= "-------<br>" . $body . "<br><br><br><br>Email: " . $email;      
+
+                $res = Mail::send('emails.send', ['title' => "Email from contact from", 'content' => $content], function ($message) 
+                {
+                    $message->subject("Hello from tienda contact");
+
+                });
+
+                $returnVal->messages[] = $success_message;
+                $returnVal->status = 'ok';
+            }
+            return response()->json($returnVal, 200);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
- use Mail;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -61,82 +60,4 @@ Route::get('/products/{slug}', 'Ecommerce\ProductsController@show');
 //Search Pages
 Route::get('/search/{term}', 'SearchController@search');
 
-Route::post('/contact', function () {
-
-	$field_rules = array(
-	    'name' => 'required',
-	    'email'   => 'required|valid_email',
-	    'message' => 'required'
-	);
-
-	// change your error messages here
-	$error_messages = array(
-	    'required'    => 'This field is required',
-	    'valid_email' => 'Please enter a valid email address'
-	);
-
-	$error_placements = array();
-
-	// success message
-	$success_message            = new stdClass();
-	$success_message->message   = 'Thank you! Your message has been sent.';
-	$success_message->field     = 'submitButton';
-
-	// mail failure message
-	$mail_error_message            = new stdClass();
-	$mail_error_message->message   = 'Sorry your mail was not sent - please try again later';
-	$mail_error_message->field     = 'submitButton';
-
-	$fields = $_POST;
-
-	$returnVal           = new stdClass();
-	$returnVal->status   = 'error';
-	$returnVal->messages = array();
-
-	if (!empty($fields)) {
-	    //Validate each of the fields
-	    foreach ($field_rules as $field => $rules) {
-	        $rules = explode('|', $rules);
-
-	        foreach ($rules as $rule) {
-	            $result = null;
-
-	            if (isset($fields[$field])) {
-	                if (!empty($rule)) {
-	                    $result = $rule($fields[$field]);
-	                }
-
-	                if ($result === false) {
-	                    $error = new stdClass();
-	                    $error->field = $field;
-	                    $error->message = $error_messages[$rule];
-	                    $error->placement = $error_placements[$field];
-
-	                    $returnVal->messages[] = $error;
-	                    // break from the rule loop so we only get 1 error at a time
-	                    break;
-	                }
-	            } else {
-	                $returnVal->messages[] =  $field . ' ' . $error_messages['required'];
-	            }
-	        }
-	    }
-
-	    if (empty($returnVal->messages)) {                         // Enable encryption, 'ssl' also accepted
-	        $email = stripslashes(safe($fields['email']));
-	        $body = stripslashes(safe($fields['message']));
-	        $content = $email . " sent you a message from your contact form:<br><br>";
-	        $content .= "-------<br>" . $body . "<br><br><br><br>Email: " . $email;      
-
-	        $res = Mail::send('emails.send', ['title' => "Email from contact from", 'content' => $content], function ($message) 
-	        {
-	            $message->subject("Hello from tienda contact");
-
-	        });
-
-	        $returnVal->messages[] = $success_message;
-	        $returnVal->status = 'ok';
-	    }
-	    return response()->json($returnVal, 200);
-	}
-});
+Route::post('/contact', 'PagesController@sendemail');
