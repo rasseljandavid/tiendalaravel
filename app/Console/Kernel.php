3a35d56525fbc1625537de8cd5dc896f76a364ae
Rassel Jan David <rasseljandavid@gmail.com>
2016-11-15 13:45:14 +0400

    Add updating of product information in the cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 use DB;
+use App\Models\Ecommerce\Product;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,9 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
-
+        // Update the quantity of the products
         $schedule->call(function () {
             $meg = new \Megaventory();
             $inv = $meg->getInventory();
@@ -36,6 +35,43 @@ class Kernel extends ConsoleKernel
                 DB::update("UPDATE products SET quantity = $item->StockPhysicalTotal where id = {$item->productID}");
             }
         })->everyMinute();
+
+     
+        $schedule->call(function () {
+            $megaventory = new \Megaventory();
+            $products = $megaventory->getProducts();
+
+            foreach($products as $product) {
+
+                $is_featured = 0;
+                $is_special  = 0;
+                $is_bestSeller = 0;
+                if(strtolower(trim($product->ProductCustomField2)) == 'featured') {
+                    $is_featured = 1;
+                }
+
+                if(strtolower(trim($product->ProductCustomField2)) == 'special') {
+                    $is_special = 1;
+                }
+
+                if(strtolower(trim($product->ProductCustomField2)) == 'bestseller') {
+                    $is_bestSeller = 1;
+                }
+                $prod = Product::find($product->ProductID);
+                $prod->is_featured   = $is_featured;
+                $prod->is_special    = $is_special;
+                $prod->is_bestSeller = $is_bestSeller;
+                $prod->rating        = rand(3,5);
+                $prod->price         = $product->ProductSellingPrice * 1.1;
+                $prod->salePrice     = $product->ProductSellingPrice;
+                $prod->update();
+            }
+        })->everyMinute();
+
+        // Update the information of the products
+        //featured
+        //price
+        //rating
     }
 
     /**
