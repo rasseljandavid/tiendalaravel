8d878c93bb788a7ee7b60142a6d48646312cd702
Rassel Jan David <rasseljandavid@gmail.com>
2016-11-16 02:55:43 +0400

    Add the start and qty for the search and remove the buttons for comparing and wishlist

--- resources/views/products/search_result.blade.php
+++ resources/views/products/search_result.blade.php
@@ -11,7 +11,7 @@
       <div class="caption">
         <h4><a href="{{ $product->slugLink() }}">{{$product->title}}</a></h4>
         <p class="description"></p>
-        <p class="price">₱
+        <p class="price"><span style="font-size: 1.2em">₱</span>
           @if($product->salePrice)
             <span class="price-new">{{ $product->salePrice }}</span> 
             <span class="price-old">{{ $product->price }}</span>
@@ -22,8 +22,21 @@
         </p>
         <!-- <p class="price"> $122.00 <span class="price-tax">Ex Tax: $100.00</span> </p> -->
       </div>
+      <p>Qty: {{$product->quantity}}</p>
+    <div class="rating"> 
+      <?php $rate = $product->rating ?>
+      @for( $i=1 ; $i <= 5; $i++)
+        <span class="fa fa-stack">
+          <i class="fa fa-star-o fa-stack-2x"></i>
+          @if($rate)
+            <i class="fa fa-star fa-stack-2x"></i>
+            <?php $rate-- ?>
+          @endif
+        </span> 
+      @endfor
+    </div>
       <div id="botton-group">
-          @include('products._options', ['type'=>'btnonly'])<br/><br>
+          <!-- @include('products._options', ['type'=>'btnonly'])<br/> -->
           <div class="cart">
             <div style="position:relative;">
               @include('cart._addtocart', ['id'=>$product->id, 'btnclass'=>'btn btn-primary btn-lg'])
