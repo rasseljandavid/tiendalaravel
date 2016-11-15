96eacbb5bc9143e1443a295c37c3a374c17c2857
Rassel Jan David <rasseljandavid@gmail.com>
2016-11-15 14:54:33 +0400

    Remove the links in the slideshow to avoid confusion for the user

--- resources/views/home/slideshow.blade.php
+++ resources/views/home/slideshow.blade.php
@@ -1,33 +1,24 @@
 <div class="slideshow single-slider owl-carousel">
 
 	<div class="item"> 
-		<a href="/category/beverages">
 			<img class="img-responsive" src="image/slider/slide1.jpg" alt="Simple Solution to your grocery shopping." title="Simple Solution to your grocery shopping." />
-		</a>
+
 	</div>
 
     <div class="item"> 
-   		<a href="/category/cupcakes-biscuits">
 			<img class="img-responsive" src="image/slider/slide2.jpg" alt="Hassle free grocery shopping." title="Hassle free grocery shopping." />
-		</a>
 	</div>
 
 	<div class="item"> 
-		<a href="/category/snacks">
 			<img class="img-responsive" src="image/slider/slide3.jpg" alt="Your groceries are only one click away." title="Your groceries are only one click away." />
-		</a>
 	</div>
 
 	<div class="item"> 
-		<a href="/category/candies-chocolate">
 			<img class="img-responsive" src="image/slider/slide4.jpg" alt="Grocery shopping at the comfort of your home." title="Grocery shopping at the comfort of your home." />
-		</a>
 	</div>
 
 	<div class="item"> 
-		<a href="/category/noodles">
 			<img class="img-responsive" src="image/slider/slide5.jpg" alt="Skip the endless line, endless traffic and shop now at tienda.ph." title="Skip the endless line, endless traffic and shop now at tienda.ph" />
-		</a>
 	</div>
     
 </div>
