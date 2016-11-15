965b03706a3e424e4203cab50fa30003681d8c8f
Rassel Jan David <rasseljandavid@gmail.com>
2016-11-15 19:11:34 +0400

    Fix the styling of the mini portrait

--- resources/views/products/mini_portrait.blade.php
+++ resources/views/products/mini_portrait.blade.php
@@ -1,12 +1,12 @@
 <div class="product-thumb clearfix">
   <div class="image"><a href="{{ $product->slugLink() }}"><img src="/image/product/{{ $product->id }}.jpg" alt="{{ $product->title }}" title="{{ $product->title }}" class="img-responsive" onerror="this.src='/image/default.jpg'" /></a></div>
   <div class="caption">
-    <h4><a href="{{ $product->slugLink() }}">{{ $product->title }}</a></h4>
+    <h4 style="margin-top: 20px; height: auto;"><a href="{{ $product->slugLink() }}">{{ $product->title }}</a></h4>
     <p class="price">
       @if($product->salePrice)
-        <span class="price-new">{{ $product->salePrice }}</span> 
+        <span style="font-size: 1.2em">₱</span><span class="price-new">{{ $product->salePrice }}</span> 
         <span class="price-old">{{ $product->price }}</span>
-        <span class="saving">-{{ $product->getSavings() }}%</span>
+        <span class="saving">-{{ $product->getSavings() }}</span>
       @else
         {{ $product->price }}
       @endif
