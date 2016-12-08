417ab8d7457387d87e2fd229e0de0cf8f0c5ce39
Nextvation <johnguild26@gmail.com>
2016-12-09 06:50:54 +0800

    fixing admin count issue

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -45,9 +45,9 @@ class AdminController extends Controller
         unset($cancelled);
 
         $ctrReceived = (count($received) ? count($received) : 0 );
-        $ctrOnProcess = Order::onProcess()->orderBy('created_at', 'desc')->count();
-        $ctrOnTransit = Order::onTransit()->orderBy('created_at', 'desc')->count();
-        $ctrShipped = Order::shipped()->orderBy('created_at', 'desc')->count();
+        $ctrOnProcess = Order::onProcess()->orderBy('created_at', 'desc')->get()->count();
+        $ctrOnTransit = Order::onTransit()->orderBy('created_at', 'desc')->get()->count();
+        $ctrShipped = Order::shipped()->orderBy('created_at', 'desc')->get()->count();
         $ctrCbc = count($cbc);
         $ctrCba = count($cba); 
 
