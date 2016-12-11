8b65ad3f0f753f2a51f4096771f671e68f5baf3d
Nextvation <johnguild26@gmail.com>
2016-12-12 07:24:01 +0800

    fixing sms notification

--- app/Http/Controllers/Ecommerce/CartController.php
+++ app/Http/Controllers/Ecommerce/CartController.php
@@ -270,7 +270,7 @@ class CartController extends Controller
 
         if(config('app.env') != 'local'){
 
-            $sms = new \SMSnotification();
+            $sms = new SMSnotification();
             $sms->send($order->user->firstname.' '.$order->user->lastname);
 
 
