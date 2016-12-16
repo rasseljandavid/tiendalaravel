4af33203561c680e79874c755583c1f4cc9dc661
Rassel Jan David <rasseljandavid@gmail.com>
2016-12-16 04:17:23 +0400

    Fix the responsiveness of the page

--- resources/views/cart/cloudstaff.blade.php
+++ resources/views/cart/cloudstaff.blade.php
@@ -125,7 +125,7 @@ window.smartsupp||(function(d) {
 </form> -->
 
               
-      <p class='grandtotal'>Total: P<span id="order-total">0.00</span></p>
+      <p class='grandtotal'><span class="hidden-xs">Total: </span>P<span id="order-total">0.00</span></p>
 
 
           </div>
@@ -513,13 +513,15 @@ $('.input-number').change(function() {
             return false;
         }
         var html = '<div class="table-responsive"><table class="table" style="text-align: left"><tbody>';
-        html    += "<tr><th class='hidden-sm hidden-xs'><strong>Name: </strong></th><td>" + $("#cname").val() + '</td></tr>';
-        html    += "<tr><th class='hidden-sm hidden-xs'><strong>Mobile: </strong></th><td>" + $("#cmobile").val() + '</td></tr>';
-        html    += "<tr><th class='hidden-sm hidden-xs'><strong>Company: </strong></th><td>" + $("#ccompany").val() + ' / ' +$("#cbranch").val() + '</td></tr>';
-        html    += "<tr><th class='hidden-sm hidden-xs'><strong>Order(s): </strong></th><td>" + orders_str + '</td></tr>';
+        html    += "<tr><th class='hidden-xs'><strong>Name: </strong></th><td>" + $("#cname").val() + '</td></tr>';
+        if($("#cmobile").val() != "") {
+        html    += "<tr><th class='hidden-xs'><strong>Mobile: </strong></th><td>" + $("#cmobile").val() + '</td></tr>';
+        }
+        html    += "<tr><th class='hidden-xs'><strong>Company: </strong></th><td>" + $("#ccompany").val() + ' / ' +$("#cbranch").val() + '</td></tr>';
+        html    += "<tr><th class='hidden-xs'><strong>Order(s): </strong></th><td>" + orders_str + '</td></tr>';
         
-        html    += "<tr><th class='hidden-sm hidden-xs'><strong>Total: </strong></th><td>P" + $("#order-total").text() + '</td></tr>';
-        html    += "<tr><th class='hidden-sm hidden-xs'><strong>Delivery: </strong></th><td>" + $("#cdeliverytime").val() + '</td></tr>';
+        html    += "<tr><th class='hidden-xs'><strong>Total: </strong></th><td>P" + $("#order-total").text() + '</td></tr>';
+        html    += "<tr><th class='hidden-xs'><strong>Delivery: </strong></th><td>" + $("#cdeliverytime").val() + '</td></tr>';
         html    += '</tbody></table></div>'
         swal({
           title: "Please Confirm Details",
